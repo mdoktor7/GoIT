68cf68fb6bf530542a24aaa612c80a9ab2891288
mdoktor7 <mdoktor696@gmail.com>
2024-05-31 21:15:58 +0300

    add fill data

--- Python/PythonWeb/module_05/SQL_Project/School/school.sql
+++ Python/PythonWeb/module_05/SQL_Project/School/school.sql
@@ -1,8 +1,4 @@
-CREATE DATABASE School;
-
-USE School;
-
--- Table of Students
+DROP TABLE IF EXISTS Students;
 CREATE TABLE Students (
     student_id INT AUTO_INCREMENT PRIMARY KEY,
     student VARCHAR(50) NOT NULL,
@@ -10,20 +6,19 @@ CREATE TABLE Students (
     FOREIGN KEY (group_id) REFERENCES Groups(group_id)
 );
 
--- Table of Groups
-CREATE TABLE Groups (
+DROP TABLE IF EXISTS Groops;
+CREATE TABLE Groops (
     group_id INT AUTO_INCREMENT PRIMARY KEY,
     group_name VARCHAR(50) NOT NULL
 );
 
--- Table of Teachers
+DROP TABLE IF EXISTS Teachers;
 CREATE TABLE Teachers (
     teacher_id INT AUTO_INCREMENT PRIMARY KEY,
     teacher VARCHAR(50) NOT NULL,
-    
 );
 
--- Table of Subjects
+DROP TABLE IF EXISTS Subjects;
 CREATE TABLE Subjects (
     subject_id INT AUTO_INCREMENT PRIMARY KEY,
     subject_name VARCHAR(50) NOT NULL,
@@ -31,7 +26,7 @@ CREATE TABLE Subjects (
     FOREIGN KEY (teacher_id) REFERENCES Teachers(teacher_id)
 );
 
--- Table of Grades
+DROP TABLE IF EXISTS Grades;
 CREATE TABLE Grades (
     grade TINYINT UNSIGNED NOT NULL 
     date_received DATE NOT NULL,
